bc07f818512a4ccffa5aa0b60ff565f4bb7e94d5
PrimozR <primozratej@gmail.com>
2020-11-02 18:30:48 +0100

    Removed duplicated scripts

--- db/DB_SCRIPT.sql
+++ db/DB_SCRIPT.sql
@@ -14,44 +14,44 @@ GRANT SELECT, INSERT, DELETE ON sirius.* TO user@'localhost';
 
 #create
 SELECT SYSDATE() as timestamp, 'Start CREATE' as decsription;
-source /home/primoz/siriusLoc/sirius/db/create/user.sql;
-source /home/primoz/siriusLoc/sirius/db/create/country.sql;
-source /home/primoz/siriusLoc/sirius/db/create/address.sql;
-source /home/primoz/siriusLoc/sirius/db/create/discount.sql;
-source /home/primoz/siriusLoc/sirius/db/create/order_status.sql;
-source /home/primoz/siriusLoc/sirius/db/create/currency.sql;
-source /home/primoz/siriusLoc/sirius/db/create/order.sql;
-source /home/primoz/siriusLoc/sirius/db/create/category.sql;
-source /home/primoz/siriusLoc/sirius/db/create/product.sql;
-source /home/primoz/siriusLoc/sirius/db/create/tag.sql;
-source /home/primoz/siriusLoc/sirius/db/create/product_details.sql;
-source /home/primoz/siriusLoc/sirius/db/create/cart_item.sql;
-source /home/primoz/siriusLoc/sirius/db/create/order_item.sql;
-source /home/primoz/siriusLoc/sirius/db/create/warehouse.sql;
-source /home/primoz/siriusLoc/sirius/db/create/inventory_items.sql;
+source C:\Users\primo\dev\sirius\db\create\user.sql;
+source C:\Users\primo\dev\sirius\db\create\country.sql;
+source C:\Users\primo\dev\sirius\db\create\address.sql;
+source C:\Users\primo\dev\sirius\db\create\discount.sql;
+source C:\Users\primo\dev\sirius\db\create\order_status.sql;
+source C:\Users\primo\dev\sirius\db\create\currency.sql;
+source C:\Users\primo\dev\sirius\db\create\order.sql;
+source C:\Users\primo\dev\sirius\db\create\category.sql;
+source C:\Users\primo\dev\sirius\db\create\product.sql;
+source C:\Users\primo\dev\sirius\db\create\tag.sql;
+source C:\Users\primo\dev\sirius\db\create\product_details.sql;
+source C:\Users\primo\dev\sirius\db\create\cart_item.sql;
+source C:\Users\primo\dev\sirius\db\create\order_item.sql;
+source C:\Users\primo\dev\sirius\db\create\warehouse.sql;
+source C:\Users\primo\dev\sirius\db\create\inventory_items.sql;
 
 #insert
 SELECT SYSDATE() as timestamp, 'Start INSERT' as decsription;
-source /home/primoz/siriusLoc/sirius/db/insert/user_v.sql;
-source /home/primoz/siriusLoc/sirius/db/insert/country_v.sql;
-source /home/primoz/siriusLoc/sirius/db/insert/address_v.sql;
-source /home/primoz/siriusLoc/sirius/db/insert/discount_v.sql;
-source /home/primoz/siriusLoc/sirius/db/insert/order_status_v.sql;
-source /home/primoz/siriusLoc/sirius/db/insert/currency_v.sql;
-source /home/primoz/siriusLoc/sirius/db/insert/order_v.sql;
-source /home/primoz/siriusLoc/sirius/db/insert/category_v.sql;
-source /home/primoz/siriusLoc/sirius/db/insert/product_v.sql;
-source /home/primoz/siriusLoc/sirius/db/insert/tag_v.sql;
-source /home/primoz/siriusLoc/sirius/db/insert/product_details_v.sql;
-source /home/primoz/siriusLoc/sirius/db/insert/cart_item_v.sql;
-source /home/primoz/siriusLoc/sirius/db/insert/order_item_v.sql;
-source /home/primoz/siriusLoc/sirius/db/insert/warehouse_v.sql;
-source /home/primoz/siriusLoc/sirius/db/insert/inventory_items_v.sql;
+source C:\Users\primo\dev\sirius\db\insert\user_v.sql;
+source C:\Users\primo\dev\sirius\db\insert\country_v.sql;
+source C:\Users\primo\dev\sirius\db\insert\address_v.sql;
+source C:\Users\primo\dev\sirius\db\insert\discount_v.sql;
+source C:\Users\primo\dev\sirius\db\insert\order_status_v.sql;
+source C:\Users\primo\dev\sirius\db\insert\currency_v.sql;
+source C:\Users\primo\dev\sirius\db\insert\order_v.sql;
+source C:\Users\primo\dev\sirius\db\insert\category_v.sql;
+source C:\Users\primo\dev\sirius\db\insert\product_v.sql;
+source C:\Users\primo\dev\sirius\db\insert\tag_v.sql;
+source C:\Users\primo\dev\sirius\db\insert\product_details_v.sql;
+source C:\Users\primo\dev\sirius\db\insert\cart_item_v.sql;
+source C:\Users\primo\dev\sirius\db\insert\order_item_v.sql;
+source C:\Users\primo\dev\sirius\db\insert\warehouse_v.sql;
+source C:\Users\primo\dev\sirius\db\insert\inventory_items_v.sql;
 
 #alter
 SELECT SYSDATE() as timestamp, 'Start ALTER' as decsription;
-source /home/primoz/siriusLoc/sirius/db/alter/alter_triggers.sql;
+source C:\Users\primo\dev\sirius\db\alter\alter_triggers.sql;
 
 #test
 SELECT SYSDATE() as timestamp, 'Start TEST' as decsription;
-source /home/primoz/siriusLoc/sirius/db/test/test_triggers.sql;
+source C:\Users\primo\dev\sirius\db\test\test_triggers.sql;
--- db/test_triggers.sql
+++
@@ -1,3 +0,0 @@
-#Test trg_before_orders_addressExistsInUser
-INSERT INTO sirius.orders(id, user_id, address_id, discount_id, order_status_code, currency_code, created, modified, amount) 
-VALUES(1,1,2,null,'AFF','ALL',SYSDATE(), SYSDATE(), 1);
