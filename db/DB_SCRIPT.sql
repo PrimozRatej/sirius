5f5bd9f18bc9f51e62e583c8bffbc4e402ccc942
PrimozR <primozratej@gmail.com>
2021-01-02 18:56:36 +0100

    DB conf on box01

--- db/DB_SCRIPT.sql
+++ db/DB_SCRIPT.sql
@@ -1,12 +1,12 @@
-DROP DATABASE sirius;
+DROP DATABASE IF EXISTS sirius;
 CREATE DATABASE sirius;
 USE sirius;
 
 DROP USER IF EXISTS 'admin'@'localhost';
 DROP USER IF EXISTS 'user'@'localhost';
 
-CREATE USER IF NOT EXISTS 'admin'@'localhost' IDENTIFIED BY 'admin';
-CREATE USER IF NOT EXISTS 'user'@'localhost' IDENTIFIED BY 'user';
+CREATE USER IF NOT EXISTS 'admin'@'localhost' IDENTIFIED BY 'admin123';
+CREATE USER IF NOT EXISTS 'user'@'localhost' IDENTIFIED BY 'user1234';
 
 GRANT ALL PRIVILEGES ON sirius.* TO 'admin'@'localhost';
 GRANT SELECT, INSERT, DELETE ON sirius.* TO user@'localhost';
@@ -14,44 +14,44 @@ GRANT SELECT, INSERT, DELETE ON sirius.* TO user@'localhost';
 
 #create
 SELECT SYSDATE() as timestamp, 'Start CREATE' as decsription;
-source C:\Users\primo\dev\sirius\db\create\user.sql;
-source C:\Users\primo\dev\sirius\db\create\country.sql;
-source C:\Users\primo\dev\sirius\db\create\address.sql;
-source C:\Users\primo\dev\sirius\db\create\discount.sql;
-source C:\Users\primo\dev\sirius\db\create\order_status.sql;
-source C:\Users\primo\dev\sirius\db\create\currency.sql;
-source C:\Users\primo\dev\sirius\db\create\order.sql;
-source C:\Users\primo\dev\sirius\db\create\category.sql;
-source C:\Users\primo\dev\sirius\db\create\product.sql;
-source C:\Users\primo\dev\sirius\db\create\tag.sql;
-source C:\Users\primo\dev\sirius\db\create\product_details.sql;
-source C:\Users\primo\dev\sirius\db\create\cart_item.sql;
-source C:\Users\primo\dev\sirius\db\create\order_item.sql;
-source C:\Users\primo\dev\sirius\db\create\warehouse.sql;
-source C:\Users\primo\dev\sirius\db\create\inventory_items.sql;
+source /home/box01/dev/sirius/db/create/user.sql;
+source /home/box01/dev/sirius/db/create/country.sql;
+source /home/box01/dev/sirius/db/create/address.sql;
+source /home/box01/dev/sirius/db/create/discount.sql;
+source /home/box01/dev/sirius/db/create/order_status.sql;
+source /home/box01/dev/sirius/db/create/currency.sql;
+source /home/box01/dev/sirius/db/create/order.sql;
+source /home/box01/dev/sirius/db/create/category.sql;
+source /home/box01/dev/sirius/db/create/product.sql;
+source /home/box01/dev/sirius/db/create/tag.sql;
+source /home/box01/dev/sirius/db/create/product_details.sql;
+source /home/box01/dev/sirius/db/create/cart_item.sql;
+source /home/box01/dev/sirius/db/create/order_item.sql;
+source /home/box01/dev/sirius/db/create/warehouse.sql;
+source /home/box01/dev/sirius/db/create/inventory_items.sql;
 
 #insert
 SELECT SYSDATE() as timestamp, 'Start INSERT' as decsription;
-source C:\Users\primo\dev\sirius\db\insert\user_v.sql;
-source C:\Users\primo\dev\sirius\db\insert\country_v.sql;
-source C:\Users\primo\dev\sirius\db\insert\address_v.sql;
-source C:\Users\primo\dev\sirius\db\insert\discount_v.sql;
-source C:\Users\primo\dev\sirius\db\insert\order_status_v.sql;
-source C:\Users\primo\dev\sirius\db\insert\currency_v.sql;
-source C:\Users\primo\dev\sirius\db\insert\order_v.sql;
-source C:\Users\primo\dev\sirius\db\insert\category_v.sql;
-source C:\Users\primo\dev\sirius\db\insert\product_v.sql;
-source C:\Users\primo\dev\sirius\db\insert\tag_v.sql;
-source C:\Users\primo\dev\sirius\db\insert\product_details_v.sql;
-source C:\Users\primo\dev\sirius\db\insert\cart_item_v.sql;
-source C:\Users\primo\dev\sirius\db\insert\order_item_v.sql;
-source C:\Users\primo\dev\sirius\db\insert\warehouse_v.sql;
-source C:\Users\primo\dev\sirius\db\insert\inventory_items_v.sql;
+source /home/box01/dev/sirius/db/insert/user_v.sql;
+source /home/box01/dev/sirius/db/insert/country_v.sql;
+source /home/box01/dev/sirius/db/insert/address_v.sql;
+source /home/box01/dev/sirius/db/insert/discount_v.sql;
+source /home/box01/dev/sirius/db/insert/order_status_v.sql;
+source /home/box01/dev/sirius/db/insert/currency_v.sql;
+source /home/box01/dev/sirius/db/insert/order_v.sql;
+source /home/box01/dev/sirius/db/insert/category_v.sql;
+source /home/box01/dev/sirius/db/insert/product_v.sql;
+source /home/box01/dev/sirius/db/insert/tag_v.sql;
+source /home/box01/dev/sirius/db/insert/product_details_v.sql;
+source /home/box01/dev/sirius/db/insert/cart_item_v.sql;
+source /home/box01/dev/sirius/db/insert/order_item_v.sql;
+source /home/box01/dev/sirius/db/insert/warehouse_v.sql;
+source /home/box01/dev/sirius/db/insert/inventory_items_v.sql;
 
 #alter
 SELECT SYSDATE() as timestamp, 'Start ALTER' as decsription;
-source C:\Users\primo\dev\sirius\db\alter\alter_triggers.sql;
+source /home/box01/dev/sirius/db/alter/alter_triggers.sql;
 
 #test
 SELECT SYSDATE() as timestamp, 'Start TEST' as decsription;
-source C:\Users\primo\dev\sirius\db\test\test_triggers.sql;
+source /home/box01/dev/sirius/db/test/test_triggers.sql;
